2ff7cad45602f9f4db3648fc3e53c14b131e9a6c
Wilson Ariza <wjarizatruquetkd@gmail.com>
2018-05-25 18:25:35 -0500

    Database and Html Template

---
+++ treeGenerator.py
@@ -0,0 +1,13 @@
+HTML_TEMPLATE = """<html>
+<head>
+<title>Tree Render:</title>
+<title>Tree Render:</title>
+<link href="base.css" rel="stylesheet" />
+</head>
+<body>
+<h1>Categories</h1>
+<pre>
+%s
+</pre>
+</body>
+</html>"""
